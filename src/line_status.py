0941d1809e3515955028d28945c792b94204d264
Omar <83602375+obejjada@users.noreply.github.com>
2023-06-18 22:52:13 +0100

    added one more unit test to test the line_status method raises an exception if an incorrect url is used

--- src/line_status.py
+++ src/line_status.py
@@ -4,5 +4,8 @@ import requests
 def line_status(url):
     '''Method to call the https://api.tfl.gov.uk/Line/Mode/{modes}/Disruption
     endpoint to return the distruption on the given line'''
-    response = requests.get(url)
-    return response.status_code, response.json()
+    try:
+        response = requests.get(url)
+        return response.status_code, response.json()
+    except Exception as e:
+        return e
--- tests/test_line_status.py
+++ tests/test_line_status.py
@@ -1,17 +1,26 @@
-from unittest import mock
+import pytest
 from src import line_status
 
 good_url_tube = 'https://api.tfl.gov.uk/Line/Mode/tube/Disruption'
 bad_url_tube = 'https://api.tfl.gov.uk/Line/Mode/flyingsaucer/Disruption'
+incorect_url_tube = 'https://api.tfl.gov.NZ/Line/Mode/tube/Disruption'
 
 
 def test_line_status_http_good_response():
-    '''Method to verify a line status HTTP response is returned 200 when the
-        URL is correct'''
+    '''Method to verify a line status method returns HTTP response of 200 when
+    the URL is correct'''
     assert line_status.line_status(good_url_tube)[0] == 200
 
 
 def test_line_status_http_bad_response():
-    '''Method to verify a line status HTTP response is returned 400 when the
-        URL is incorrect'''
+    '''Method to verify a line status method returns HTTP response of 400 when
+    the URL contains an incorrect parameter'''
     assert line_status.line_status(bad_url_tube)[0] == 400
+
+
+def test_line_status_incorect_url():
+    '''Method to verify a line status method raises a TypeError expection when
+    an incorrect URL is used'''
+    with pytest.raises(Exception) as exc_info:
+        line_status.line_status(incorect_url_tube)
+        assert exc_info == TypeError
